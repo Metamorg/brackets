50b1f58a39ba1bc54b3353ddeacfdcc5ec02b1e9
Metamorg <egoronokasd@gmail.com>
2021-02-16 00:14:18 +0300

    fix: fix brackets

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ module.exports = function check(str, bracketsConfig) {
   for(let i = 0; i < arr.length; i++) {
     if(str.includes(arr[i])) {
       str = str.replace(arr[i], '');
-      i =- 1;
+      i = -1;
     }
   }
   return str.length === 0 ? true : false
